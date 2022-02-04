a1aa4a5054d71ccc8c6f4cdbcdc75e8bb7296570
groyce <groyce@unomaha.edu>
2022-02-04 15:34:23 -0600

    heroku site added

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = "https://movies-groyce.pythonanywhere.com/";  /*"http://127.0.0.1:8000/"; */
+const API_URL = "https://movie-vue3.herokuapp.com/";/*"https://movies-groyce.pythonanywhere.com/";  /*"http://127.0.0.1:8000/"; */
 
 export class APIService {
     constructor() {
